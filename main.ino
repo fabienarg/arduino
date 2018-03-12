adcebde882a5ba3305dda9063ba8f4e677a2adeb
fabienarg <argelierf@gmail.com>
2018-03-12 17:48:28 +0100

    RAS

--- main.ino
+++ main.ino
@@ -6,9 +6,14 @@
 // the setup function runs once when you press reset or power the board
 void setup()
 {
+    pinMode(8,OUTPUT);
 }
 
 // the loop function runs over and over again forever
 void loop()
 {
+    digitalWrite(8,HIGH);
+    delay(2000);
+    digitalWrite(8,LOW);
+    delay(2000);
 }
